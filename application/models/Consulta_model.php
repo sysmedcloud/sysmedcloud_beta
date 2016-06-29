3d2316872bf8b9f6446a450da1ec2f5e46119c91
cristian <cristian@Vidal-PC>
2016-06-29 00:23:56 -0400

    Modulo Consulta Medica (19062016)

--- application/models/Consulta_model.php
+++ application/models/Consulta_model.php
@@ -13,4 +13,88 @@ class Consulta_model extends CI_Model
         $this->db->insert('tbl_consulta_medica',$data);
         return $this->db->insert_id();
     }
+    
+    public function add_rev_sint_generales($data){
+        
+        $this->db->insert('tbl_rs_sintomas_generales',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_rev_sint_resp($data){
+        
+        $this->db->insert('tbl_rs_sintomas_respiratorios',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_rev_sint_cardio($data){
+        
+        $this->db->insert('tbl_rs_sintomas_cardiovasculares',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_rev_sint_gastro($data){
+        
+        $this->db->insert('tbl_rs_sintomas_gastroinstestinales',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_rev_sint_geni($data){
+        
+        $this->db->insert('tbl_rs_sintomas_genitourinarios',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_rev_sint_neuro($data){
+        
+        $this->db->insert('tbl_rs_sintomas_neurologicos',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_rev_sint_endo($data){
+        
+        $this->db->insert('tbl_rs_sintomas_endocrinos',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_decubito($data){
+        
+        $this->db->insert('tbl_efg_decubito',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_deambulacion($data){
+        
+        $this->db->insert('tbl_efg_deambulacion',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_facie($data){
+        
+        $this->db->insert('tbl_efg_facie',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_conciencia($data){
+        
+        $this->db->insert('tbl_efg_conciencia',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_piel($data){
+        
+        $this->db->insert('tbl_efg_piel',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_s_linfatico($data){
+        
+        $this->db->insert('tbl_efg_linfatico',$data);
+        return $this->db->insert_id();
+    }
+    
+    public function add_ex_signos_vitales($data){
+        
+        $this->db->insert('tbl_efg_signos_vitales',$data);
+        return $this->db->insert_id();
+    }
 }	
